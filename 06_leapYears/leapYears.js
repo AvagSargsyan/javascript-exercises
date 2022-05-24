1e217550a97432864a533449de17d0d1b68b9240
avag <avagsargsyan01@gmail.com>
2022-05-24 15:15:19 +0400

    Solved exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(yearNum) {
+    if (yearNum % 100 === 0) {
+        if (yearNum % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (yearNum % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
