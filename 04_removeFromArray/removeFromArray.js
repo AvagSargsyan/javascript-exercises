a3670dead8357180f9b56859fd2c15a252015d06
avag <avagsargsyan01@gmail.com>
2022-05-24 14:50:35 +0400

    Solved exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let resultArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let exists = false;
+        for (let j = 0; j < args.length; j++) {
+            if (arr[i] === args[j]) {
+                exists = true;
+            }
+        }
+        if (!exists) {
+            resultArr.push(arr[i]);
+        }
+    }
+    return resultArr;
 };
 
 // Do not edit below this line
