d8dc17aad7a8884b6ddf1d96c0d3f626e8e8e6b2
avag <avagsargsyan01@gmail.com>
2022-06-04 20:45:40 +0400

    Solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let item;
+    let result = [1, 1];
+    
+    if (num < 0) {
+        return 'OOPS';
+    }
 
+    if (num === 0) {
+        return 0;
+    }
+
+    if (num === 1 || num === 2) {
+        return 1;
+    }
+    
+    else {
+        for (let i = 1; i < num - 1; i++) {
+            item = result[i - 1] + result[i];
+            result.push(item);
+        }
+        return result[num - 1];
+    }
 };
+
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
