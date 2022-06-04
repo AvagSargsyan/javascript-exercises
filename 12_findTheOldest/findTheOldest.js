bc7cc8d6e05ff0542bc99477f2a1aa5d1ad9c159
avag <avagsargsyan01@gmail.com>
2022-06-04 21:13:26 +0400

    Solved exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = (new Date()).getFullYear();
+    return arr.map(personObj => {
+        if (personObj.yearOfDeath !== undefined) {
+            return {
+                name: personObj.name,
+                age: personObj.yearOfDeath - personObj.yearOfBirth
+            }
+        } else {
+            return {
+                name: personObj.name,
+                age: currentYear - personObj.yearOfBirth
+            }
+        }
+    }).reduce((aggr, obj) => {
+        if (aggr.age < obj.age) {
+            aggr = obj;
+        } else {
+            return aggr;
+        }
+        return aggr;
+    });
 };
 
 // Do not edit below this line
