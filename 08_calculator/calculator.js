9d538d6c19cfd4f3758b09424442564aed555bca
avag <avagsargsyan01@gmail.com>
2022-06-04 19:23:50 +0400

    Solved exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((aggr, val) => aggr + val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let result = 1;
+  for (let num of args) {
+    result *= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	let result = base;
+  while (exp > 1) {
+    result *= base;
+    exp--;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  while (num > 1) {
+    result *= num;
+    num--;
+  }
+  return result;
 };
 
 // Do not edit below this line
